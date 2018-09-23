0ad607dc91a6a89e60bb6f1ce5da4528e54d7a4b
Nick Pascetti <Npascetti@gmail.com>
2018-09-23 01:01:03 -0600

    removed logo, added some onclick functionality to button, logging text

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1
+    })
+    const message = "You clicked it!!" + this.state.count;
+    console.log(message)
+  }
+
   render () {
     return (
       <div className='button__container'>
-        <button className='button'>Click Me</button>
+        <button className='button' onClick = {this.handleClick}>Click Me</button>
       </div>
     )
   }
