8960acb374126d15760ab1e0901dd35092beb141
Nick Pascetti <Npascetti@gmail.com>
2018-09-23 00:36:26 -0600

    removed boilerplate, added basic button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Sending shit to the DOM
-
-        </p>
+      <div className='button__container'>
+        <button className='button'>Click Me</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
